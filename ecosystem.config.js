a0bac5244ac6cfee63a8a94effbf791fefb97f31
Danielle Blom <dlblom91@gmail.com>
2020-09-22 11:42:10 -0500

    Prepare for deployment with new EC2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'Blom_footer',
-    script: './server/index.js'
-  }],
+  apps: [
+    {
+      name: "Blom_footer",
+      script: "./server/index.js",
+    },
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-18-222-223-167.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/tutorial.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:ATX-50-Team-Best-Buy/Blom_footer.git',
-      path: '/home/ubuntu/blom_footer',
-      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
-    }
-  }
-}
+      user: "ubuntu",
+      host: "ec2-3-17-139-201.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/tutorial.pem",
+      ref: "origin/master",
+      repo: "git@github.com:ATX-50-Team-Best-Buy/Blom_footer.git",
+      path: "/home/ubuntu/blom_footer",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js",
+    },
+  },
+};
